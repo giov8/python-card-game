d364c5cdf9080c329fc59adf96911ec9627e36f7
giov8 <giovani.ggm@gmail.com>
2025-06-11 17:05:46 -0300

    create class

---
+++ main.py
@@ -0,0 +1,8 @@
+from personagem import Personagem
+
+objeto_personagem1 = Personagem("Rodrigo Estudioso")
+mensagem = objeto_personagem1.falar_nome()
+print(mensagem)
+
+while True:
+    input()
---
+++ personagem.py
@@ -0,0 +1,6 @@
+class Personagem:
+    def __init__(self, nome):
+        self.nome = nome
+        self.vida_maxima = 100
+        self.vida = 100
+        
